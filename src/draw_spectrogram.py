b0e43f50b6e6c2b47dbf3ebbf5663c6372ba0616
ArthurFDLR <arthfind@gmail.com>
2021-03-12 12:17:23 -0600

    🎨 Improve spectrogram visualization

--- src/draw_spectrogram.py
+++ src/draw_spectrogram.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 import seaborn as sns
 import numpy as np
 
@@ -14,22 +15,24 @@ print(config)
 freq_lim = 3000. #Hz
 df = pd.read_csv(spectrogram_path, sep=',',header=None, skiprows=1)
 spectrogram = df.to_numpy()[:,0:int(freq_lim/float(config['delta_f']))].transpose()
+spectrogram = np.log(spectrogram)
 
 rcstyle = {'axes.linewidth': 1.0, 'axes.edgecolor': 'black','ytick.minor.size': 5.0}
 sns.set(font_scale=2.0)
 sns.set_style('ticks', rcstyle)
 
 plt.figure(figsize=(18, 10))
-colors = sns.color_palette("Blues", as_cmap=True)
-ax = sns.heatmap(spectrogram, cmap=colors, cbar = False)
+colors_audacity = [(1, 1, 1), (1, 1, 1), (.37, .71, 1), (1., .15, .20), (1, .85, .73), (1, .95, .95)]
+cmap = LinearSegmentedColormap.from_list("audacity", colors_audacity, N=100)
+ax = sns.heatmap(spectrogram, cmap=cmap, cbar = False)
 
 ax.set_xlabel('Time [s]')
 ax.set_xticks(np.linspace(0, spectrogram.shape[1], 10))
 ax.set_xticklabels(["{0:.1f}".format(x) for x in np.linspace(0.0, float(config['delta_t']) * spectrogram.shape[1], 10)])
 
 ax.set_ylabel('Frequency [Hz]')
-ax.set_yticks(np.linspace(0, 1000, 10))
-ax.set_yticklabels([str(int(x)) for x in np.linspace(0.0, float(config['delta_f'])  * freq_lim, 10)])
+ax.set_yticks(np.linspace(0, spectrogram.shape[0], 10))
+ax.set_yticklabels([str(int(x)) for x in np.linspace(0.0, spectrogram.shape[0] * float(config['delta_f']), 10)])
 ax.invert_yaxis()
 
 for side in ['left', 'right', 'bottom', 'top']:
--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,8 @@ int main(int argc, char** argv)
 	size_t F_s = a.getSampleRate();
 	double delta_t = 1./F_s;
 	size_t N = sample.size();
-	size_t window_size = 16384;
-	size_t window_step = window_size/10;
+	size_t window_size = pow(2, 12);
+	size_t window_step = window_size/8;
 	double delta_f = double(F_s)/window_size;
 
 	Fft::mat_complex spectrogram = Fft::stft_fft(sample, window_size, window_step);
